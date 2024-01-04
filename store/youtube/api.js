3e4a5d2c6138083dff8ac7fd4803030e663e3336
Crowdbotics <engineering@crowdbotics.com>
2024-01-04 08:36:52 +0000

    Update store.

--- store/youtube/api.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios"
-const youtube = axios.create({
-  baseURL: "https://www.youtube.com/",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
-export const apiService = {}
